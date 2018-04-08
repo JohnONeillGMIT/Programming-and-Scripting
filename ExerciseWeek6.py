704e1b29fd0108b26f5f7a282164ea125c6f49dd
JohnONeillGMIT <36233106+JohnONeillGMIT@users.noreply.github.com>
2018-04-08 10:20:55 +0100

    Add files via upload

--- ExerciseWeek6.py
+++ ExerciseWeek6.py
@@ -1,5 +1,6 @@
 #John O'Neill 08/04/2016
-#Creating a Function to retuen a Factorial of an Integer (eg 5*4*3*2*1)
+#--------Factorials---------
+#Creating a Function to return a Factorial of an Integer (eg 5*4*3*2*1)
 #Create Variables,get the input,loop in the function , make it generic!
 
 
@@ -9,8 +10,8 @@ def Factorial(n): #Function takes an input to the function
     Sum *= i # *= adds number to itself (sum= sum*i)
   return Sum #return keyword gives the output of a function
 
-n = int(input("Please enter an Integer: "))
+n = int(input("Please enter an Integer: ")) #asking for the integer input to drive the def
 
-print("The Result for Factorial:" Factorial(n))
+print("The Result for Factorial is :", Factorial(n)) #prints the result of the function.
 
 
