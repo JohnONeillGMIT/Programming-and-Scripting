1d58949cc25a4a71e0c9f3b6ef626af857f2808a
JohnONeillGMIT <36233106+JohnONeillGMIT@users.noreply.github.com>
2018-04-08 19:40:08 +0100

    Update ExerciseWeek4.py

--- ExerciseWeek4.py
+++ ExerciseWeek4.py
@@ -2,7 +2,7 @@
 #John O'Neill 01/04/2018
 #Euler Problem 5... Finding Lowest common multiple for a range 1-20
 #'''Used much of logic from https://www.youtube.com/watch?v=Km36RkQToqo and adjusted some variable inputs based on other knowledge on LCM from other sources'''
-
+# https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers
 
 
 def LCM(sum):
