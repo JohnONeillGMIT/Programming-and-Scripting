73491580a4e87f28f2dd76a9060bdce5388d6f31
JohnONeillGMIT <36233106+JohnONeillGMIT@users.noreply.github.com>
2018-04-08 10:59:39 +0100

    Add files via upload

--- fib.py
+++ fib.py
@@ -1,4 +1,5 @@
-# Ian McLoughlin
+# John O'Neill amended 08/04/2108 to add comments
+# ...Using coding of Ian McLoughlin.
 
 # A program that displays Fibonacci numbers.
 
@@ -6,14 +7,14 @@ def fib(n):
   """This function returns the nth Fibonacci number."""
   i = 0
   j = 1
-  n = n - 1
+  n = n - 1 #sets ini
 
   while n >= 0:
-    i, j = j, i + j
+    i, j = j, i + j #used to step through the Fib iterations
     n = n - 1
   return i
 
 # Test the function with the following value.
-x = 30
+x = 20
 ans = fib(x)
 print("Fibonacci number", x, "is", ans)
